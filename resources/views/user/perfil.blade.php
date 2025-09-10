51a7c24d7af39fc27f7956dcbddadb0035edbe46
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-09-10 09:48:57 -0500

    Instalación de paquete LANG y cambio de zona horaria

--- resources/views/user/perfil.blade.php
+++ resources/views/user/perfil.blade.php
@@ -5,7 +5,7 @@
 @section('contenido')
     <div class="flex items-center justify-center my-15 px-3">
         <div class="w-full max-w-md">
-            <h1 class="text-center text-2xl font-bold mb-6">Bienvenido, Jorge Luis</h1>
+            <h1 class="text-center text-2xl font-bold mb-6">Bienvenido, {{ Auth::user()->name }}</h1>
         </div>
     </div>
 @endsection
